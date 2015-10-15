613a6be69153efaf6ef213a499acf237105dbde6
Anand Upadhyay <anandu@gumromedia.com>
2015-10-15 09:06:58 -0400

    removed comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914191653) do
+ActiveRecord::Schema.define(version: 20151014181258) do
 
   create_table "carriers", force: :cascade do |t|
     t.string   "name"
