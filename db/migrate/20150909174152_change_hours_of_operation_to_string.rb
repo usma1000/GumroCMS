c78cd0edc33749a35fda962bbb4f054139e55f6c
Anand Upadhyay <anandu@gumromedia.com>
2015-10-30 15:39:55 -0400

    fixing migration

--- db/migrate/20150909174152_change_hours_of_operation_to_string.rb
+++ db/migrate/20150909174152_change_hours_of_operation_to_string.rb
@@ -3,6 +3,6 @@ class ChangeHoursOfOperationToString < ActiveRecord::Migration
     change_column :facilities, :hours_of_operation, :string
   end
   def down
-    change_column :facilities, :hours_of_operation, :integer
+    change_column :facilities, :hours_of_operation, 'integer USING CAST(hours_of_operation AS integer)'
   end
 end
