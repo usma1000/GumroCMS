69a7373929ecd42539b296d1b8cf5c22f5563426
Anand Upadhyay <anandu@gumromedia.com>
2015-10-30 15:32:52 -0400

    fixed migration

--- db/migrate/20151030132418_add_columns_to_carriers.rb
+++ db/migrate/20151030132418_add_columns_to_carriers.rb
@@ -5,6 +5,6 @@ class AddColumnsToCarriers < ActiveRecord::Migration
     add_column :carriers, :participate_in_recycling_programs, :boolean
     add_column :carriers, :scanning, :boolean
     add_column :carriers, :scanning_software, :string
-    remove_column :carriers, :operate_twentyfour_hours
+    remove_column :carriers, :operate_twentyfour_hours, :boolean
   end
 end
